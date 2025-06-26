320edf67dd22432acee9976ad758e8980d9c896b
András Risnyovszký <andras@forbestclients.com>
2025-06-26 11:17:08 +0200

    revert changes

--- components/credits/CreditsUnauth.jsx
+++ components/credits/CreditsUnauth.jsx
@@ -1,30 +1,31 @@
-import styles from "@/modules/cart/components/credits/CreditsUnauth.module.scss";
-import Button from "@/components/_other/button/Button";
-import LoginForm from "@/components/forms/login/LoginForm";
-import {useState} from "react";
+import styles from '@/modules/cart/components/credits/CreditsUnauth.module.scss';
+import Button from '@/components/_other/button/Button';
+import LoginForm from '@/components/forms/login/LoginForm';
+import { useState } from 'react';
 
 const CreditsUnauth = () => {
+  const [loginToggle, setLoginToggle] = useState(false);
 
-    const [loginToggle, setLoginToggle] = useState(false);
+  const handleLoginToggleButton = () => {
+    setLoginToggle(!loginToggle);
+  };
 
-    const handleLoginToggleButton = () => {
-        setLoginToggle(!loginToggle);
-    };
-
-    return (
-        <>
-            <div className={styles.login}>
-                Pre uplatnenie a využívanie kreditov sa musíte prihlásiť do svojho
-                konta.
-                <div className={styles.button}>
-                    <Button type="ghost" onClick={handleLoginToggleButton}>Prihlásiť sa</Button>
-                </div>
-            </div>
-            <div className={loginToggle ? 'block' : 'hidden'}>
-                <LoginForm />
-            </div>
-        </>
-    )
-}
+  return (
+    <>
+      <div className={styles.login}>
+        Pre uplatnenie a využívanie kreditov sa musíte prihlásiť do svojho
+        konta.
+        <div className={styles.button}>
+          <Button type="ghost" onClick={handleLoginToggleButton}>
+            Prihlásiť sa
+          </Button>
+        </div>
+      </div>
+      <div className={loginToggle ? 'block' : 'hidden'}>
+        <LoginForm />
+      </div>
+    </>
+  );
+};
 
 export default CreditsUnauth;
