036f28ba76393cdfda9a7672283690a203c740b7
Pavol Rasev <pavol.rasev@gmail.com>
2025-10-16 11:52:34 +0200

    MINICART - visual updates

--- components/AmountInputCart.jsx
+++ components/AmountInputCart.jsx
@@ -1,28 +1,35 @@
 import { startTransition } from 'react';
-import NumberInput from "@/components/_other/form/NumberInput";
-import debounce from "lodash/debounce";
+import NumberInput from '@/components/_other/form/NumberInput';
+import debounce from 'lodash/debounce';
 
 const AmountInputCart = ({
-   amount,
-   setAmount,
-   afterChange,
-   loading = false
- }) => {
+                           amount,
+                           setAmount,
+                           afterChange,
+                           loading = false,
+                         }) => {
   const MAX = 999;
 
   const handleChange = debounce((value) => {
     if (value !== amount) {
       const newValue = value > MAX ? MAX : value;
-      startTransition(()=> {
-        setAmount?.(newValue || 1),
-          afterChange?.(newValue);
-        }
+      startTransition(() => {
+          setAmount?.(newValue || 1),
+            afterChange?.(newValue);
+        },
       );
     }
   }, 500);
 
   return (
-      <NumberInput key={amount} onValueChange={handleChange} loading={loading} showButtons={true}  inputProps={{ name: 'amount', groupClassName: '!mb-0 !mt-0', readOnly: true, min: 1, max: MAX, value: amount }} hasFeedback={false} />
+    <NumberInput key={amount} onValueChange={handleChange} loading={loading} showButtons={true} inputProps={{
+      name: 'amount',
+      groupClassName: '!m-0',
+      readOnly: true,
+      min: 1,
+      max: MAX,
+      value: amount,
+    }} hasFeedback={false} />
   );
 };
 
--- components/minicart/MiniCart.jsx
+++ components/minicart/MiniCart.jsx
@@ -30,46 +30,81 @@ export const MiniCart = ({ isOpen, handleClose }) => {
     }
   }, [isOpen]);
 
+  const onMiniCartOpen = () => {
+    setIsEnabled(true);
+  };
+
+  useEffect(() => {
+    document.addEventListener('miniCartOpen', onMiniCartOpen);
+
+    return () => {
+      document.removeEventListener('miniCartOpen', onMiniCartOpen);
+    };
+  });
+
   if (!isEnabled) {
     return '';
   }
 
   return createPortal(
-      <div className={`fixed left-0 top-0 w-full h-full z-50 bg-grey/70 transition-all backdrop-blur-sm
-      duration-500 pointer-events-none ${!isOpen ? 'opacity-0' : 'opacity-1'}`}>
-        <div
-          className={`
-            absolute
-            bottom-0 w-full h-fit
-            sm:right-0 sm:w-[600px] sm:h-full
-            bg-white
-            px-6 sm:px-12 pb-6 pt-8
-            flex flex-col
-            overflow-auto
-            pointer-events-auto
-            transition-all
-            duration-1000
-            ${isOpen ? 'translate-0 translate-y-0' : 'sm:translate-x-full translate-y-full sm:translate-y-0'}
-          `}
-        >
-          <span className={'absolute right-[50px] top-7 flex items-center justify-center'}>
-            <Button type="text" onClick={handleClose}>
-              <CloseIcon className={'h-6 text-grey/80 hover:text-font w-auto'} />
-            </Button>
-          </span>
-          <div>
-            <h2 className={'flex gap-8 items-center text-xl font-semibold'}>
-              <CartIcon className={'h-8 w-auto'} />
+    <div
+      className={`fixed left-0 top-0 w-full h-full z-50 bg-grey/70 transition-all backdrop-blur-sm
+      duration-500 pointer-events-none ${!isOpen ? 'opacity-0' : 'opacity-1'}`}
+    >
+      <div
+        className={`
+      absolute
+      bottom-0 w-full
+      sm:right-0 sm:w-[600px] sm:h-full
+      bg-white
+      px-6 sm:px-6 pb-6 pt-6
+      flex flex-col inset-y-0
+      pointer-events-auto
+      transition-all
+      duration-1000
+      ${
+          isOpen
+            ? 'translate-0 translate-y-0'
+            : 'sm:translate-x-full translate-y-full sm:translate-y-0'
+        }
+      `}
+      >
+        <div className="flex flex-row items-center justify-between mb-2">
+          <h3 className={'grid grid-cols-8 gap-x-4 md:gap-x-8 items-center text-lg md:text-xl flex-wrap mb-0 mr-5'}>
+            <CartIcon className={'h-6 md:h-8 w-auto flex-shrink-0'} />
+            <span className="col-span-6 md:col-span-7">
               {t('cart.your_cart')}
-            </h2>
-            <Divider className="my-8" />
-            <MiniCartItems />
-          </div>
-          <Button type={'primary'} fullWidth  className="mt-6" htmlType={'a'} href={CART_STEPS[0]} icon={<CartIcon />}>
-            {t('cart.proceed_to_cart')}
+            </span>
+          </h3>
+          <Button
+            type="text"
+            onClick={handleClose}
+          >
+            <CloseIcon className={'h-6 md:h-8 w-auto'} />
           </Button>
         </div>
-      </div>,
-      document.getElementById('mini-cart-drawer'),
+        <Divider className="mt-2 mb-1" />
+        <div className={'flex-1 overflow-y-auto py-0 px-4'}>
+          <MiniCartItems />
+        </div>
+        <div className={'flex flex-col justify-end flex-shrink-0 w-full'}>
+          <Divider />
+          <Button
+            fullWidth
+            htmlType={'a'}
+            href={CART_STEPS[0]}
+            size="medium"
+          >
+            {t('cart.proceed_to_cart')}
+            <CartIcon className="ml-3" />
+          </Button>
+
+          <button onClick={handleClose} className="mt-4 hover:underline text-base">
+            {t('cart.close')}
+          </button>
+        </div>
+      </div>
+    </div>,
+    document.getElementById('mini-cart-drawer'),
   );
 };
